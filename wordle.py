a1562973f23e1cb7dffd055d1008d39e0e80f77b
George V. Reilly <george@reilly.org>
2023-02-16 20:42:18 -0800

    cleanup

--- wordle.py
+++ wordle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
-import sys
 
 parser = argparse.ArgumentParser(description="Wordle Finder")
 parser.set_defaults(
-    wordfile="/usr/share/dict/words",
+    word_file="/usr/share/dict/words",
     len=5,
 )
+parser.add_argument("--verbose", "-v", action="store_true", help="Show all the steps")
 parser.add_argument(
-    "--verbose", "-v", action="store_true",
-    help="Show all the steps")
-parser.add_argument(
-    "guess", nargs="+", metavar="WORD=guess",
-    help="Examples: 'ARISE=.r.se' 'ROUTE=R.u.e' 'RULES=Ru.eS'")
+    "guess",
+    nargs="+",
+    metavar="WORD=guess",
+    help="Examples: 'ARISE=.r.se' 'ROUTE=R.u.e' 'RULES=Ru.eS'",
+)
 namespace = parser.parse_args()
 
 
@@ -22,7 +22,7 @@ def debug(s):
         print(s)
 
 
-with open(namespace.wordfile) as f:
+with open(namespace.word_file) as f:
     WORDS = [w.upper() for w in f.read().splitlines() if len(w) == namespace.len]
 
 # 'ADIEU=a.i..' 'CLOTH=c....'
@@ -36,6 +36,8 @@ mask = [None] * namespace.len  # Exact match for position (Green)
 wrong_spot = [set() for _ in range(namespace.len)]  # Wrong spot (Yellow)
 for guess in namespace.guess:
     word, result = guess.split("=")
+    assert len(word) == namespace.len
+    assert len(result) == namespace.len
     for i, (w, r) in enumerate(zip(word, result)):
         assert "A" <= w <= "Z", "WORD should be uppercase"
         if "A" <= r <= "Z":
@@ -47,8 +49,7 @@ for guess in namespace.guess:
         elif r == ".":
             invalid.add(w)
         else:
-            print(f"Unexpected {r} for {w}")
-            sys.exit(1)
+            raise ValueError(f"Unexpected {r} for {w}")
 
 debug(f"{invalid=}")
 debug(f"{valid=}")
@@ -60,17 +61,14 @@ for w in WORDS:
     letters = {c for c in w}
     if letters & valid != valid:
         debug(f"!Valid: {w}")
-        continue
-    if letters & invalid:
+    elif letters & invalid:
         debug(f"Invalid: {w}")
-        continue
-    if any(m is not None and c != m for c, m in zip(w, mask)):
+    elif any(m is not None and c != m for c, m in zip(w, mask)):
         debug(f"!Mask: {w}")
-        continue
-    if any(c in ws for c, ws in zip(w, wrong_spot)):
+    elif any(c in ws for c, ws in zip(w, wrong_spot)):
         debug(f"WrongSpot: {w}")
-        continue
-    choices.append(w)
-    debug(f"Got: {w}")
+    else:
+        choices.append(w)
+        debug(f"Got: {w}")
 
 print("\n".join(choices))

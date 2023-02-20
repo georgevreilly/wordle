baefdb7052a2b34754e01e73c763d15b0156f2dd
George Reilly <george@reilly.org>
2023-02-19 23:25:10 -0800

    Weight by position

--- startwords.py
+++ startwords.py
@@ -9,76 +9,60 @@ parser.set_defaults(
     # word_file="/usr/share/dict/words",
     word_file="wordle.txt",
     len=5,
-    topmost=30,
+    topmost=50,
+    top_per_letter=20,
+    threshold_score=0.30,
 )
 namespace = parser.parse_args()
 
-# https://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
-LETTER_PROBABILITIES = {
-    "E": 12.02,
-    "T": 9.10,
-    "A": 8.12,
-    "O": 7.68,
-    "I": 7.31,
-    "N": 6.95,
-    "S": 6.28,
-    "R": 6.02,
-    "H": 5.92,
-    "D": 4.32,
-    "L": 3.98,
-    "U": 2.88,
-    "C": 2.71,
-    "M": 2.61,
-    "F": 2.30,
-    "Y": 2.11,
-    "W": 2.09,
-    "G": 2.03,
-    "P": 1.82,
-    "B": 1.49,
-    "V": 1.11,
-    "K": 0.69,
-    "X": 0.17,
-    "Q": 0.11,
-    "J": 0.10,
-    "Z": 0.07,
-}
-
-AtoZ = range(ord('A'), ord('Z')+1)
+AtoZ = [chr(c) for c in range(ord("A"), ord("Z") + 1)]
 
 
 with open(namespace.word_file) as f:
     WORDS = [w.upper() for w in f.read().splitlines() if len(w) == namespace.len]
 
-letter_counts = {chr(c): 0 for c in AtoZ}
+letter_counts = {c: 0 for c in AtoZ}
 total_letters = 0
+pos_pop = {c: [0 for _ in range(namespace.len)] for c in AtoZ}
+
 for w in WORDS:
     total_letters += len(w)
-    for c in w:
+    for i,c in enumerate(w):
         letter_counts[c] += 1
+        pos_pop[c][i] += 1
 
-frequencies = {l: round(c/total_letters, 3)
-               for l,c in sorted(letter_counts.items(), reverse=True, key=itemgetter(1))}
+frequencies = {
+    l: round(c / total_letters, 3)
+    for l, c in sorted(letter_counts.items(), reverse=True, key=itemgetter(1))
+}
 print(frequencies)
+for letter, positions in pos_pop.items():
+    print(f"{letter}: {positions}")
+print()
 
 start_words = defaultdict(list)
-alpha_words = {chr(c): [] for c in AtoZ}
+alpha_words = {c: [] for c in AtoZ}
 
 for w in WORDS:
     letters = {c for c in w}
     if len(letters) == namespace.len:
         score = sum(letter_counts[c] for c in w) / total_letters
+        score += sum(pos_pop[c][i] for i, c in enumerate(w)) / total_letters
         start_words["".join(sorted(letters))].append((w, score))
         alpha_words[w[0]].append((w, score))
 
 print(f"{len(start_words)}/{len(WORDS)} words with {namespace.len} distinct letters")
-topwords = sorted(start_words.items(), reverse=True, key=lambda kv: kv[1][0][1])[:namespace.topmost]
+topwords = sorted(start_words.items(), reverse=True, key=lambda kv: kv[1][0][1])[
+    : namespace.topmost
+]
 for i, x in enumerate(topwords, 1):
     anagrams = [ws[0] for ws in x[1]]
     print(f"{i:2}: {' '.join(anagrams)}")
 
 print()
 
-for letter in (chr(c) for c in AtoZ):
-    topwords = sorted(alpha_words[letter], reverse=True, key=itemgetter(1))[:namespace.topmost]
-    print(f"{letter}: {' '.join(ws[0] for ws in topwords if ws[1] >= 0.30)}")
-
+for letter in AtoZ:
+    topwords = sorted(alpha_words[letter], reverse=True, key=itemgetter(1))[
+        : namespace.top_per_letter
+    ]
+    print(f"{letter}: {' '.join(ws[0] for ws in topwords if ws[1] >= namespace.threshold_score)}")

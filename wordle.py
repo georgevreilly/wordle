fab2ec8df85cd49c9bfe2b32626f38acf6d5a846
George Reilly <george@reilly.org>
2023-04-08 23:05:02 -0700

    Remove stale code

--- wordle.py
+++ wordle.py
@@ -19,36 +19,6 @@ parser.add_argument(
 )
 namespace = parser.parse_args()
 
-# https://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
-LETTER_PROBABILITIES = {
-    "E": 12.02,
-    "T": 9.10,
-    "A": 8.12,
-    "O": 7.68,
-    "I": 7.31,
-    "N": 6.95,
-    "S": 6.28,
-    "R": 6.02,
-    "H": 5.92,
-    "D": 4.32,
-    "L": 3.98,
-    "U": 2.88,
-    "C": 2.71,
-    "M": 2.61,
-    "F": 2.30,
-    "Y": 2.11,
-    "W": 2.09,
-    "G": 2.03,
-    "P": 1.82,
-    "B": 1.49,
-    "V": 1.11,
-    "K": 0.69,
-    "X": 0.17,
-    "Q": 0.11,
-    "J": 0.10,
-    "Z": 0.07,
-}
-
 
 def debug(s):
     if namespace.verbose:
@@ -62,11 +32,6 @@ def trace(s):
 
 with open(namespace.word_file) as f:
     WORDS = [w.upper() for w in f.read().splitlines() if len(w) == namespace.len]
-
-# 'ADIEU=a.i..' 'CLOTH=c....'
-# 'RAISE=r...e' 'CLOUT=.L...' 'NYMPH=.....' 'ELVER=EL.ER'
-# 'GRAIL=.RA..' 'TRACK=.RAc.' 'CRAMP=CRA..' 'CRABS=CRA..' 'CRAZY=CRAZ.'
-# 'ARISE=.r.se' 'ROUTE=R.u.e' 'RULES=Ru.eS'
 
 invalid = set()  # Gray
 valid = set()  # Green or Yellow

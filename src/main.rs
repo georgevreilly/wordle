cd453ad118abd6c5f0eba7b5387a38cc4de67404
George V. Reilly <george@reilly.org>
2023-10-26 23:14:49 -0700

    Mimic Python GuessScore in Rust

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,70 @@ struct Args {
 const WORD_FILE: &str = "wordle.txt";
 const LEN: usize = 5;
 
+#[derive(PartialEq, Copy, Clone, Debug)]
+enum TileState {
+    CORRECT,
+    PRESENT,
+    ABSENT,
+}
+
+#[derive(Debug)]
+struct GuessScore {
+    guess: String,
+    #[allow(dead_code)]
+    score: String,
+    tiles: [TileState; LEN],
+}
+
+fn tile_state(score_tile: u8) -> Result<TileState> {
+    return if (b'A'..=b'Z').contains(&score_tile) {
+        Ok(TileState::CORRECT)
+    } else if (b'a'..=b'z').contains(&score_tile) {
+        Ok(TileState::PRESENT)
+    } else if score_tile == b'.' {
+        Ok(TileState::ABSENT)
+    } else {
+        Err(anyhow!("Invalid score {:?}", score_tile))
+    };
+}
+
+fn parse_guess_score(guess_score: &str) -> Result<GuessScore> {
+    if let Some((guess, score)) = guess_score.split_once('=') {
+        if guess.len() != LEN {
+            return Err(anyhow!(format!("Guess {guess} is not {LEN} characters")));
+        }
+        if score.len() != LEN {
+            return Err(anyhow!(format!("Score {score} is not {LEN} characters")));
+        }
+        let mut tiles = [TileState::CORRECT; LEN];
+        for i in 0..LEN {
+            let g: u8 = guess.as_bytes()[i];
+            let s: u8 = score.as_bytes()[i];
+            if !(b'A'..=b'Z').contains(&g) {
+                return Err(anyhow!("Guess {:?} should be uppercase", guess));
+            }
+            let state = tile_state(s)?;
+            if state == TileState::CORRECT {
+                if g != s {
+                    return Err(anyhow!("Mismatch at {}: {}!={}", i + 1, guess, score));
+                }
+            } else if state == TileState::PRESENT {
+                if s - b'a' != g - b'A' {
+                    return Err(anyhow!("Mismatch at {}: {}!={}", i + 1, guess, score));
+                }
+            }
+            tiles[i] = state
+        }
+        return Ok(GuessScore {
+            guess: guess.to_owned(),
+            score: score.to_owned(),
+            tiles,
+        });
+    } else {
+        return Err(anyhow!(format!("Expected one '=' in '{guess_score}'")));
+    }
+}
+
 struct ParsedGuesses {
     valid: HashSet<u8>,
     invalid: HashSet<u8>,
@@ -36,47 +100,30 @@ impl ParsedGuesses {
     }
 }
 
-fn parse_guesses(guesses: &Vec<String>) -> Result<ParsedGuesses> {
+fn parse_guesses(guess_scores: &Vec<GuessScore>) -> Result<ParsedGuesses> {
     let mut pg = ParsedGuesses::new();
-    for guess in guesses {
-        if let Some((word, result)) = guess.split_once('=') {
-            if word.len() != LEN {
-                return Err(anyhow!("{:?} is not {} characters long", word, LEN));
-            } else if result.len() != LEN {
-                return Err(anyhow!("{:?} is not {} characters long", result, LEN));
+    for gs in guess_scores {
+        // First pass for correct and present
+        for i in 0..LEN {
+            let g: u8 = gs.guess.as_bytes()[i];
+            let t = gs.tiles[i];
+            if t == TileState::CORRECT {
+                pg.mask[i] = g;
+                pg.valid.insert(g);
+            } else if t == TileState::PRESENT {
+                pg.wrong_spot[i].insert(g);
+                pg.valid.insert(g);
             }
-            for i in 0..LEN {
-                let w: u8 = word.as_bytes()[i];
-                let r: u8 = result.as_bytes()[i];
-                if !(b'A'..=b'Z').contains(&w) {
-                    return Err(anyhow!("{:?} should be uppercase", word));
-                }
-                if (b'A'..=b'Z').contains(&r) {
-                    if r != w {
-                        return Err(anyhow!(
-                            "Mismatch at position {} between {:?} and {:?}",
-                            i,
-                            word,
-                            result
-                        ));
-                    }
-                    pg.valid.insert(w);
-                    pg.mask[i] = w;
-                } else if (b'a'..=b'z').contains(&r) {
-                    if r - b'a' != w - b'A' {
-                        return Err(anyhow!(
-                            "Mismatch at position {} between {:?} and {:?}",
-                            i,
-                            word,
-                            result
-                        ));
-                    }
-                    pg.valid.insert(w);
-                    pg.wrong_spot[i].insert(w);
-                } else if r == b'.' {
-                    pg.invalid.insert(w);
+        }
+        // Second pass for absent letters
+        for i in 0..LEN {
+            let g: u8 = gs.guess.as_bytes()[i];
+            let t = gs.tiles[i];
+            if t == TileState::ABSENT {
+                if pg.valid.contains(&g) {
+                    pg.wrong_spot[i].insert(g);
                 } else {
-                    return Err(anyhow!("Invalid result char: {:?}", r));
+                    pg.invalid.insert(g);
                 }
             }
         }
@@ -84,8 +131,8 @@ fn parse_guesses(guesses: &Vec<String>) -> Result<ParsedGuesses> {
     Ok(pg)
 }
 
-fn solve(words: &Vec<String>, guesses: &Vec<String>) -> Result<Vec<String>> {
-    let pg = parse_guesses(guesses)?;
+fn solve(words: &Vec<String>, guess_scores: &Vec<GuessScore>) -> Result<Vec<String>> {
+    let pg = parse_guesses(guess_scores)?;
     info!("valid: {:?}", pg.valid);
     info!("invalid: {:?}", pg.invalid);
     info!("mask: {:?}", pg.mask);
@@ -131,7 +178,14 @@ fn main() -> Result<()> {
         .filter(|w| w.len() == LEN)
         .map(|w| w.to_uppercase())
         .collect::<Vec<String>>();
-    let choices = solve(&words, &args.guesses)?;
+    let guess_scores: Result<Vec<GuessScore>> = args
+        .guesses
+        .into_iter()
+        .map(|gs| parse_guess_score(&gs))
+        .collect();
+    let guess_scores = guess_scores.unwrap();
+    // println!("guess_scores = {:?}", guess_scores);
+    let choices = solve(&words, &guess_scores)?;
     println!(
         "{}",
         if choices.is_empty() {

b15955b0b65962d8959977d8389b472249cf5f86
George Reilly <georgevreilly@stripe.com>
2023-10-26 21:51:24 -0700

    Better pycheck

--- pycheck.sh
+++ pycheck.sh
@@ -2,11 +2,14 @@
 
 set -euxo pipefail
 
-DIR="$(dirname $0)"
-ruff "$DIR"
-isort --check "$DIR"
-black "$DIR"
-flake8 "$DIR"
-mypy "$DIR"
+# Code quality checks
+all_files=(./*.py)
+
+black --check "${all_files[@]}"
+flake8 "${all_files[@]}"
+ruff "${all_files[@]}"
+isort --check "${all_files[@]}"
+mypy "${all_files[@]}"
 
 # TODO: pytest
+# pytest -p no:cacheprovider test_*.python

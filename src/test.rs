144081b3dda670eeb940c428f368157c6b085053
George V. Reilly <george@reilly.org>
2023-10-31 22:06:54 -0700

    More tests

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,43 @@
 use super::*;
 
 #[test]
+fn test_tile_state() -> Result<()> {
+    assert_eq!(TileState::CORRECT, TileState::make(b'Q')?);
+    assert_eq!(TileState::PRESENT, TileState::make(b'q')?);
+    assert_eq!(TileState::ABSENT, TileState::make(b'.')?);
+    assert!(TileState::make(b'@').is_err());
+    Ok(())
+}
+
+#[test]
+fn test_parse_guess_score() {
+    let gs = GuessScore::parse("TRACK=.RAc.").unwrap();
+    assert_eq!("TRACK", gs.guess);
+    assert_eq!(".RAc.", gs.score);
+    assert_eq!(
+        vec![
+            TileState::ABSENT,
+            TileState::CORRECT,
+            TileState::CORRECT,
+            TileState::PRESENT,
+            TileState::ABSENT
+        ],
+        gs.tiles
+    );
+    assert_eq!("⬛🟩🟩🟨⬛", gs.emojis(""));
+}
+
+#[test]
+fn test_formatters() {
+    let set1 = HashSet::from([b'R', b'A']);
+    let set2 = HashSet::from([b'E', b'T', b'C']);
+    assert_eq!("AR", letter_set(&set1));
+    let sets = vec![set1, set2];
+    assert_eq!("[AR,CET]", letter_sets(&sets));
+    assert_eq!("-AR--", dash_mask(&[b'\0', b'A', b'R', b'\0', b'\0']));
+}
+
+#[test]
 fn test_parse_game_result() {
     let line =
         "* 186: `GRAIL=.RA.. TRACK=.RAc. CRAMP=CRA.. CRABS=CRA.. CRAZY=CRAZ.` yields `CRAZE`";
@@ -15,5 +52,8 @@ fn test_parse_game_result() {
 #[test]
 fn test_score() -> Result<()> {
     assert_eq!(".RAc.", WordleGuesses::score("CRAZE", "TRACK")?);
+    assert_eq!("AR..Y", WordleGuesses::score("ARRAY", "ARTSY")?);
+    assert_eq!(".RR..", WordleGuesses::score("ARRAY", "ERROR")?);
+    assert_eq!("r..OR", WordleGuesses::score("ERROR", "RUMOR")?);
     Ok(())
 }

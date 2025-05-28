c995d9cd863c35a732a8a9120320e4e242ccc4a8
George V. Reilly <george@reilly.org>
2025-05-27 23:56:43 -0700

    Add ty and pyrefly

--- pycheck.sh
+++ pycheck.sh
@@ -7,9 +7,12 @@ all_files=(./*.py)
 
 black --check "${all_files[@]}"
 flake8 "${all_files[@]}"
-ruff check "${all_files[@]}"
 isort --check "${all_files[@]}"
+ruff check "${all_files[@]}"
+
 mypy "${all_files[@]}"
+# ty check "${all_files[@]}"
+pyrefly check "${all_files[@]}"
 
 # TODO: pytest
 # pytest -p no:cacheprovider test_*.python
